8c3971f721f1c76e9a6f8f5636179e345b6b9337
CaC <sakema7@gmail.com>
2022-09-25 17:59:06 -0300

    v0.1

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -9,20 +9,38 @@ function proccess(e) {
 
     // Definir variable
     let _userNumber;
+    const _n0 = 0;
+    const _n1 = 1;
+    const _n2 = 2;
 
     // Establecer valor de la variable '_userNumber'
 
+    // Verificar si el número es un entero
     do {
-        _userNumber = parseInt(prompt(`Ingresa un número entero.`));
+        do {
+            _userNumber = parseFloat(prompt(`Ingresa un número entero.`));
 
-        if (isNaN(_userNumber)) {
-            console.log(`El caracter ingresado no es un número.`);
+            // Verificar si el caracter ingresado es un número
+            if (isNaN(_userNumber)) {
+                alert(`No se ha ingresado no un número.`);
+            }
+        } while (isNaN(_userNumber));
+
+        // Verificar si el número ingresado es un entero
+        
+        if (_userNumber % _n1 != _n0) {
+            alert(`El número ingresado no es un entero.\n`);
         }
-    } while (isNaN(_userNumber));
+    } while (_userNumber % _n1 != _n0);
 
-    alert(`El`);
+    console.log(`Número ingresado: ${_userNumber}`);
 
-
+    // Verificar si el número ingresado es par o impar
+    if (_userNumber % _n2 != _n0) {
+        console.log(`${_userNumber} es un número impar.\n————————————————————`);
+    } else {
+        console.log(`${_userNumber} es un número par.\n————————————————————`);
+    }
 }
 
 _start.onsubmit = proccess;
