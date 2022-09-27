ea4dcc91d69d541623ece9829c1f1ef2ee4c3a92
CaC <sakema7@gmail.com>
2022-09-27 12:46:40 -0300

    v1.1

--- js/ejercicio09.js
+++ js/ejercicio09.js
@@ -5,162 +5,138 @@ function iniciar(e) {
     console.log(`\n————————————————————\nEjercicio #09\n————————————————————`);
 
     // Definir variables
-    let _angA;          // Primer ángulo
-    let _angB;          // Segundo ángulo
-    let _angC;          // Tercer ángulo
-    let _angSuma;       // Suma de todos los ángulos
+    let _ladoA;          // Primer ángulo
+    let _ladoB;          // Segundo ángulo
+    let _ladoC;          // Tercer ángulo
     let _triangulo;     // Tipo de triángulo
     let _temp;          // Variable temporal
     const _n0 = 0;      // Número 0
     const _n1 = 1;      // Número 1
-    const _n90 = 90;    // Número 90
-    const _n180 = 180;  // Número 180
 
-    // Establecer el valor de las variables '_angA', '_angB' y '_angC'
+    // Establecer el valor de las variables '_ladoA', '_ladoB' y '_ladoC'
 
-    alert(`A continuación deberá ingresar el valor númerico de los 3 ángulos de un triángulo.\nRecuerde que la suma de los 3 ángulos debe ser igual a 180°.`)
+    alert(`A continuación deberá ingresar el la longitud de los 3 lados de un triángulo.\nRecuerde que para que un triángulo sea válido, cada uno de sus lados no puede ser más largo que la suma de los otros dos.`)
 
+    // Ingresar el primer lado
     do {
         do {
             do {
                 // Ingresar el valor de A
-                _angA = prompt(`Ingrese el valor del primer ángulo.\nEl número debe ser un entero positivo.`);
-                if (_angA == null) {
+                _ladoA = prompt(`Ingrese el valor del primer ángulo.\nEl número debe ser un entero positivo.`);
+                if (_ladoA == null) {
                     console.log(`Ejercicio cancelado.`);
                     return;
                 }
     
                 // Verificar si el caracter ingresado es un número
-                if (isNaN(_angA)) {
+                if (isNaN(_ladoA)) {
                     alert(`No se ha ingresado no un número.`);
                 }
-            } while (isNaN(_angA));
+            } while (isNaN(_ladoA));
     
             // Verificar si el número ingresado es un entero
-            if (_angA % _n1 != _n0) {
+            if (_ladoA % _n1 != _n0) {
                 alert(`El número ingresado no es un entero.\n`);
             }
-        } while (_angA % _n1 != _n0);
+        } while (_ladoA % _n1 != _n0);
 
         // Verificar si el número ingresado es un entero
-        if (_angA < _n1) {
+        if (_ladoA < _n1) {
             alert(`El número ingresado debe ser positivo.\n`);
         }
-    } while (_angA < _n1);
+    } while (_ladoA < _n1);
 
-    // Calcular los grados restantes
-    _temp = _n180 - _angA;
-
-    // Establecer el segundo ángulo
+    // Establecer el segundo lado
     do {
         do {
             do {
-                if (_angA >= _n180) {
-                    // Ingresar el valor de B
-                    _angB = prompt(`Ingrese el valor del segundo ángulo.\nEl número debe ser un entero positivo.`);
-                    if (_angB == null) {
-                        console.log(`Ejercicio cancelado.`);
-                        return;
-                    }
-
-                } else {
-                    // Ingresar el valor de B
-                    _angB = prompt(`Ingrese el valor del segundo ángulo.\nEl número debe ser un entero positivo.\n\nSugerencia:\nIngrese un número entre ${_n1} y ${_temp - _n1} (inclusive).`);
-                    if (_angB == null) {
-                        console.log(`Ejercicio cancelado.`);
-                        return;
-                    }
+                // Ingresar el valor de B
+                _ladoB = prompt(`Ingrese el valor del segundo ángulo.\nEl número debe ser un entero positivo.`);
+                if (_ladoB == null) {
+                    console.log(`Ejercicio cancelado.`);
+                    return;
                 }
 
                 // Verificar si el caracter ingresado es un número
-                if (isNaN(_angB)) {
+                if (isNaN(_ladoB)) {
                     alert(`No se ha ingresado no un número.`);
                 }
-            } while (isNaN(_angB));
+            } while (isNaN(_ladoB));
     
             // Verificar si el número ingresado es un entero
-            if (_angB % _n1 != _n0) {
+            if (_ladoB % _n1 != _n0) {
                 alert(`El número ingresado no es un entero.\n`);
             }
-        } while (_angB % _n1 != _n0);
+        } while (_ladoB % _n1 != _n0);
 
         // Verificar si el número ingresado es un entero
-        if (_angB < _n1) {
+        if (_ladoB < _n1) {
             alert(`El número ingresado debe ser positivo.\n`);
         }
-    } while (_angB < _n1);
+    } while (_ladoB < _n1);
 
-    // Calcular los grados restantes
-    _temp = _temp - _angB;
-
-    // Establecer el tercer ángulo
+    // Establecer el tercer lado
     do {
         do {
             do {
-                if (_temp < _n1) {
-                    // Ingresar el valor de C
-                    _angC = prompt(`Ingrese el valor del tercer ángulo.\nEl número debe ser un entero positivo.`);
-                    if (_angC == null) {
-                        console.log(`Ejercicio cancelado.`);
-                        return;
-                    }
-
-                } else {
-                    // Ingresar el valor de C
-                    _angC = prompt(`Ingrese el valor del tercer ángulo.\nEl número debe ser un entero positivo.\n\nSugerencia:\nIngrese el número ${_temp}.`);
-                    if (_angC == null) {
-                        console.log(`Ejercicio cancelado.`);
-                        return;
-                    }
+                // Ingresar el valor de C
+                _ladoC = prompt(`Ingrese el valor del tercer ángulo.\nEl número debe ser un entero positivo.`);
+                if (_ladoC == null) {
+                    console.log(`Ejercicio cancelado.`);
+                    return;
                 }
 
                 // Verificar si el caracter ingresado es un número
-                if (isNaN(_angC)) {
+                if (isNaN(_ladoC)) {
                     alert(`No se ha ingresado no un número.`);
                 }
-            } while (isNaN(_angC));
+            } while (isNaN(_ladoC));
     
             // Verificar si el número ingresado es un entero
-            if (_angC % _n1 != _n0) {
+            if (_ladoC % _n1 != _n0) {
                 alert(`El número ingresado no es un entero.\n`);
             }
-        } while (_angC % _n1 != _n0);
+        } while (_ladoC % _n1 != _n0);
 
         // Verificar si el número ingresado es un entero
-        if (_angC < _n1) {
+        if (_ladoC < _n1) {
             alert(`El número ingresado debe ser positivo.\n`);
         }
-    } while (_angC < _n1);
+    } while (_ladoC < _n1);
 
     // parseInt evita un problema al comprar con números negativos
-    _angA = parseInt(_angA);
-    _angB = parseInt(_angB);
-    _angC = parseInt(_angC);
+    _ladoA = parseInt(_ladoA);
+    _ladoB = parseInt(_ladoB);
+    _ladoC = parseInt(_ladoC);
 
-    // Verificar si el triángulo es posible
-    _angSuma = _angA + _angB + _angC;
+    // Verificar si el triángulo es un error
+    if (_ladoA > (_ladoB + _ladoC)) {
+        // El triángulo es inválido
+        _temp = false;
 
-    if (_angSuma > _n180) {
-        alert(`El valor ingresado para los ángulos es incorrecto.\nLa suma de los 3 ángulos es mayor a 180°.`);
-        console.log(`El valor ingresado para los ángulos es incorrecto.\nLa suma de los 3 ángulos es mayor a 180°.`);
-        return;
+    } else if (_ladoB > (_ladoA + _ladoC)) {
+        // El triángulo es inválido
+        _temp = false;
 
-    } else if (_angSuma < _n180) {
-        alert(`El valor ingresado para los ángulos es incorrecto.\nLa suma de los 3 ángulos es menor a 180°.`);
-        console.log(`El valor ingresado para los ángulos es incorrecto.\nLa suma de los 3 ángulos es mnor a 180°.`);
-        return;
+    } else if (_ladoC > (_ladoA + _ladoB)) {
+        // El triángulo es inválido
+        _temp = false;
+    } else {
+        // El triángulo es válido
+        _temp = true;
     }
 
-    // Verificar que tipo de triángulo se ingreso
-    if (_angA == _n90 || _angB == _n90 || _angC == _n90) {
-        // Se ingresó un triángulo rectángulo
-        _triangulo = "rectángulo";
 
-    } else if (_angA == _angB && _angA == _angC) {
+    // Verificar que tipo de triángulo se ingreso
+    if (_temp == false) {
+        // Se ingresó un triángulo rectángulo
+        _triangulo = "inválido";
+
+    } else if (_ladoA == _ladoB && _ladoA == _ladoC) {
         // Se ingresó un triángulo equilátero
         _triangulo = "equilátero";
 
-    } else if (_angA != _angB && _angA != _angC && _angB != _angC) {
+    } else if (_ladoA != _ladoB && _ladoA != _ladoC && _ladoB != _ladoC) {
         // Se ingresó un triángulo escaleno
         _triangulo = "escaleno";
 
@@ -172,10 +148,10 @@ function iniciar(e) {
     // Presentar datos
     console.log(`
         ————————————————————
-        1er ángulo: ${_angA}
-        2do ángulo: ${_angB}
-        3er ángulo: ${_angC}
-        Se ingresó un triángulo ${_triangulo}
+        1er lado: ${_ladoA}
+        2do lado: ${_ladoB}
+        3er lado: ${_ladoC}
+        Se ingresó un triángulo ${_triangulo}.
         ————————————————————`);
 }
 
